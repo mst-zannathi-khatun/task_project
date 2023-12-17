c4e235ca21ee15818486c1d6da9c4879df387fba
zannathi <m.zannathi.k@gmail.com>
2023-12-17 07:17:24 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const TaskProject());
+}
+
+class TaskProject extends StatelessWidget {
+  const TaskProject({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+
+    );
+  }
+}
